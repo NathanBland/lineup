63cd296a65b36fe03c2124885a4039deb776cc7b
Nathan Bland <nathan.bland@gmail.com>
2016-02-09 04:18:06 +0000

    Fixed Twitter auth some magical way

--- routes/auth/routes.js
+++ routes/auth/routes.js
@@ -14,31 +14,15 @@ router.get('/login', function (req, res, next) {
  * 
 **/
 router.get('/login/twitter', passport.authenticate('twitter'))
-router.get('/login/twitter/return', function(req, res, next) {
-  console.log('[auth/routes.js](/login/twitter/return) hit twitter cb')
-  passport.authenticate('twitter', function (err, user, info) {
-    console.log('authenticating..')
-    if (err) { 
-      console.log('err:', err)
-      return next(err) 
-    }
-    if (user) {
-      console.log('user:', user)
-    }
-    console.log('info:', info)
-    if (!user) { return res.redirect('/login') }
-    req.logIn(user, function (err) {
-      if (err) {
-        return next(err)
-      }
-      return res.redirect('/test')
-    })(req, res, next)
-  })
+
+router.get('/login/twitter/return',
+  passport.authenticate('twitter', { 
+    successRedirect: '/',
+    failureRedirect: '/login' }))
   /*passport.authenticate('twitter', {
     successRedirect: '/test',
     failureRedirect: '/login'
   })*/
-})
 /**
  * 
  * End Twitter
